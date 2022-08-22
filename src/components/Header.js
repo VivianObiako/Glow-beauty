d740fcad1fc4a6cc4c489d86985f6bfef4475f14
Oobiako-vertex <82474446+Oobiako-vertex@users.noreply.github.com>
2022-08-22 16:15:33 +0100

    styled site header

---
+++ src/components/Header.js
@@ -0,0 +1,101 @@
+import React, { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { BellIcon, MenuIcon, XIcon, ShoppingBagIcon } from '@heroicons/react/outline'
+
+const navigation = [
+  { name: 'Product', href: '#', current: false },
+  { name: 'About', href: '#', current: false },
+  { name: 'Blog', href: '#', current: false },
+  { name: 'Reviews', href: '#', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+const Header = () => {
+  return (
+    <Disclosure as="nav" className="bg-bg">
+      {({ open }) => (
+        <>
+          <div className="max-w-6xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16 md:mt-7">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6 " aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex-shrink-0 flex items-center">
+                    <h1 className='font-reco text-4xl text-blue'>Glow</h1>
+                    <div className='logo-dot bg-orange ml-1 mt-3'></div>
+                </div>
+                
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <div className="hidden sm:block sm:ml-6 md:mr-9">
+                    <div className="flex space-x-4">
+                        {navigation.map((item) => (
+                        <a
+                            key={item.name}
+                            href={item.href}
+                            className={classNames(
+                            item.current ? 'bg-gray-900 text-white' : 'text-light-black hover:bg-gray-700 hover:text-white',
+                            'px-3 py-2 rounded-md text-base font-medium'
+                            )}
+                            aria-current={item.current ? 'page' : undefined}
+                        >
+                            {item.name}
+                        </a>
+                        ))}
+                    </div>
+                </div>
+                <div>
+                    <button
+                    type="button"
+                    className="bg-orange p-2 relative rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white h-10 w-10"
+                    >
+                    <span className="sr-only">Shop</span>
+                    <ShoppingBagIcon className="h-5 w-5 text-text-white m-auto" aria-hidden="true" />
+                    <p className=' absolute text-text-white text-xs w-5 rounded-full bg-blue border top-[-4px] right-[-3px]'>1</p>
+                    </button>
+
+                </div>
+                
+
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'bg-gray-900 text-white' : 'text-text-white bg-blue hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
+
+
+export default Header
