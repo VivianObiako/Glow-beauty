5dafb3bed3e34e398038d03cdaec783e4d9cc2f3
Oobiako-vertex <82474446+Oobiako-vertex@users.noreply.github.com>
2022-08-25 00:46:29 +0100

    fixed mobile nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
             <div className="relative flex items-center justify-between h-16 md:mt-7">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:mid-blue hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-mid-blue">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <HiX className="block h-6 w-6 " aria-hidden="true" />
